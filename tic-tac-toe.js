dc78762b93f3222c47e8209c3d991398fe635e9a
Albert <albertcampos08@gmail.com>
2016-11-28 11:24:43 -0800

    circles

---
+++ tic-tac-toe.js
@@ -0,0 +1,79 @@
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 10)
+circle.setAttribute("cy", 10)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+
+
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 120)
+circle.setAttribute("cy", 10)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+
+
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 230)
+circle.setAttribute("cy", 10)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+
+
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 10)
+circle.setAttribute("cy", 120)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+
+
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 10)
+circle.setAttribute("cy"230)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+
+
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 120)
+circle.setAttribute("cy", 120)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+
+
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 230)
+circle.setAttribute("cy",120)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+
+
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 120)
+circle.setAttribute("cy", 230)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+
+
+var namespace = "http://www.w3.org/2000/svg"
+var canvas = document.getElementById("my-canvas")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 23)
+circle.setAttribute("cy", 230)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
