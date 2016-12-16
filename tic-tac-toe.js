4a9b13228784ba67d6abb60b67a922408e9bccba
Albert <albertcampos08@gmail.com>
2016-12-15 16:23:43 -0800

    ijndsa

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,4 @@
+
 var namespace = "http://www.w3.org/2000/svg"
 var currentPlayer = 1
 var topleftclick = false
@@ -5,12 +6,27 @@ function game(){
   if (currentPlayer == 1){
     if (topleftclick == false) {
       var circle = document.createElementNS(namespace, "circle")
-      circle.setAttribute("cx", "0")
-      circle.setAttribute("cy", "0")
+      circle.setAttribute("cx", "50")
+      circle.setAttribute("cy", "50")
       circle.setAttribute("r", "50")
-      circle.setAttribute("fill", "grey")
+      circle.setAttribute("fill", "black")
       var canvas = document.getElementById("game-board")
       canvas.appendChild(circle)
       topleftclick= true
       currentPlayer= 2
   }
+}else if (currentPlayer == 2){
+  if (topleftclick == false) {
+    var square = document.createElementNS(namespace, "rect")
+    square.setAttribute("x", "10")
+    square.setAttribute("y", "10")
+    square.setAttribute("height", "80")
+    square.setAttribute("width", "80")
+    square.setAttribute("fill", "grey")
+    var canvas = document.getElementById("game-board")
+    canvas.appendChild(square)
+    topleftfclick= true
+    currentPlayer= 1
+    }
+  }
+}
